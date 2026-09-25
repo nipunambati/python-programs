8b48de73d4e3b3736a8bb6b18f47c9ed1698a6c3
nipunambati <26ambatins@rbunagpur.in>
2026-09-25 23:17:37 +0530

    Update all programs.py

--- all programs.py
+++ all programs.py
@@ -23,3 +23,32 @@ string = "nipun"
 for char in string:
     print(char)
 
+Q-4
+
+text = "Python is Fun!"
+vowels = "aeiou"
+vowel_list = [char for char in text if char in vowels]
+print("Vowels found:", vowel_list)
+print("Total number of vowels:", len(vowel_list))
+
+Q-5 
+
+s = "Count the spaces in this string."
+spaces = s.count(" ")
+print(spaces)
+
+Q-6 
+
+s = "nipun"
+reversed_s = s[::-1]
+print(reversed_s)
+
+Q-7
+
+text = "banana"
+char_to_count = "a"
+count = text.count(char_to_count)
+print(f"The character '{char_to_count}' appears {count} times.")
+
+
+
