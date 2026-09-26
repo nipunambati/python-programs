e864af6e6600916fb60b7d795f87f2876dfbe7ce
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 11:01:57 +0530

    Update Q-21.py

--- Q-21.py
+++ Q-21.py
@@ -1,1 +1,11 @@
+Q-21 Find greater of two numbers
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater.")
+elif num2 > num1:
+    print(f"{num2} is greater.")
+else:
+    print("Both numbers are equal.")
