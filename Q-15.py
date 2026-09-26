743b1fdcb5ed29bf78979677143c3319c9124a10
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 10:59:39 +0530

    Update Q-15.py

--- Q-15.py
+++ Q-15.py
@@ -1,1 +1,15 @@
+Q-15 Calculate total and percentage of 5 subjects
+
+eng = float(input("eng: "))
+hindi = float(input("hindi: "))
+maths = float(input("maths: "))
+python = float(input("python: "))
+sci = float(input("sci: "))
+
+total_marks = eng + hindi + maths + python + sci
+percentage = (total_marks / 500) * 100
+
+print("\n--- Results ---")
+print(f"Total Marks Obtained: {total_marks} / 500")
+print(f"Percentage: {percentage:.2f}%")
 
