f0cfbcc1fe8af9df2a69abb3ddff734de4330926
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 10:56:23 +0530

    Update Q-4.py

--- Q-4.py
+++ Q-4.py
@@ -1,1 +1,7 @@
+Q-4 Count vowels
 
+text = "Python is Fun!"
+vowels = "aeiou"
+vowel_list = [char for char in text if char in vowels]
+print("Vowels found:", vowel_list)
+print("Total number of vowels:", len(vowel_list))
