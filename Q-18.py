cbc349583bc9e2d4c4a0f83466c40e657f34ff1e
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 11:00:50 +0530

    Update Q-18.py

--- Q-18.py
+++ Q-18.py
@@ -1,1 +1,10 @@
+Q-18 Check whether a number is positive, negative or zero
 
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
