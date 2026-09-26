5d547e2992ac7636763f1706850d5f65e812c781
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 11:01:38 +0530

    Update Q-20.py

--- Q-20.py
+++ Q-20.py
@@ -1,1 +1,9 @@
+Q-20 Check whether a person is eligible to vote
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
 
