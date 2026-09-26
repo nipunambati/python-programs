505dcf354184c425f8e3b265078769a082ac00a5
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 11:00:25 +0530

    Update Q-17.py

--- Q-17.py
+++ Q-17.py
@@ -1,1 +1,11 @@
+Q-17 Convert seconds into hours, minutes and seconds
+
+total_seconds = 10000
+
+hours = total_seconds // 3600
+remaining_seconds = total_seconds % 3600
+minutes = remaining_seconds // 60
+seconds = remaining_seconds % 60
+
+print(f"{hours} hours, {minutes} minutes, and {seconds} seconds")
 
