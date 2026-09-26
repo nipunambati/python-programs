df2befa564dfc7f4ab9f75ed5ab131d9f4ef4ede
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 10:58:19 +0530

    Update Q-11.py

--- Q-11.py
+++ Q-11.py
@@ -1,1 +1,12 @@
+Q-10 Check whether a string is palindrome
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+word = "Racecar"
+if is_palindrome(word):
+    print(f"'{word}' is a palindrome")
+else:
+    print(f"'{word}' is not a palindrome")
 
