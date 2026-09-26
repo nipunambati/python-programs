0a550d8ff1f923082ab142ac55c126cac2689b2f
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 11:05:17 +0530

    Update Q-30.py

--- Q-30.py
+++ Q-30.py
@@ -1,1 +1,8 @@
+Q-30 Check whether a number lies between 10 and 50
 
+num = int(input("Enter a number: "))
+
+if num >= 10 and num <= 50:
+    print("The number lies between 10 and 50")
+else:
+    print("The number does not lie between 10 and 50")
