e4bc9b49a252c516f74060e61882d7449cfadddb
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 11:00:05 +0530

    Update Q-16.py

--- Q-16.py
+++ Q-16.py
@@ -1,1 +1,14 @@
+Q-16 Swap two numbers
 
+a = 10
+b = 20
+
+print(f"The value of a is {a}")
+print(f"The value of b is {b}")
+
+temp = a
+a = b
+b = temp
+
+print(f"The value after swapping of a is {a}")
+print(f"The value after swapping of b is {b}")
