9328fc15672ee6b9a6cb87edb3c143e97e7e0e2a
nipun sai dev ambati <26ambatins@rbunagpur.in>
2026-09-26 10:59:18 +0530

    Update Q-14.py

--- Q-14.py
+++ Q-14.py
@@ -1,1 +1,7 @@
+Q-14 Convert Celsius to Fahrenheit
 
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius}°C is equal to {fahrenheit:.2f}°F")
